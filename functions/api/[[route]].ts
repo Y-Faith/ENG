8daa8401048a98bdb9798e8c0b859a13fdaa90f7
Y-Faith <your-email@example.com>
2026-05-18 22:09:49 +0800

    fix route: remove /api prefix for pages functions

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -2,6 +2,6 @@ import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 
 const app = new Hono()
-app.get('/api/test', (c) => c.text('hono works'))
+app.get('/test', (c) => c.text('hono works'))
 
 export const onRequest = handle(app)
